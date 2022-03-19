901715fa72d1b58df3741b1b8945479693075d1e
Gracia Marca <gracia_marca@usp.pe>
2022-03-19 12:55:44 -0500

    calculadora

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using sum.Models;
+
+namespace sum.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Privacy(int a,int b,int c)
+        {   
+            ViewBag.op1 = a;
+            ViewBag.op2 = b;
+            var resul = 0;
+            
+            if(c == 1){
+                resul = a + b;
+            }else if(c == 2){
+                resul = a - b;
+            }else if(c == 3){
+                resul = a * b;
+            }else if(c == 4){
+                resul = a / b;
+            }
+
+            ViewBag.resul = resul;
+            
+            return View();
+        }
+        
+        
+                
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Operacion.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sum.Models
+{
+    public class Operacion
+    {
+        public int op1 { get; set; }
+        public int op2 { get; set; }
+        public int operacion { get; set; }
+        public double resul { get; set; }
+    }
+}
